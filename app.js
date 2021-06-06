6b7cba9d3e7c6c603173bd9bc6739546b8d5a55a
umutcan <akturkumutcan58@gmail.com>
2021-06-06 12:59:57 +0300

    commit

--- app.js
+++ app.js
@@ -10,8 +10,16 @@ app.use(express.static(path.join(__dirname,'public')));
 
 
 app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.get('/sign',(req,res)=>{
     res.render('sign');
-})
+});
+
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
 
 app.listen(3000,()=>{
     console.log("port çalışıyor");
